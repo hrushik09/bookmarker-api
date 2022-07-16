a919e296e2fba2653002171086f1a28d0f361ef0
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2022-07-16 12:00:34 +0530

    added BookmarksDTO

--- src/main/java/io/hrushik09/bookmarker/api/BookmarkController.java
+++ src/main/java/io/hrushik09/bookmarker/api/BookmarkController.java
@@ -1,14 +1,12 @@
 package io.hrushik09.bookmarker.api;
 
-import io.hrushik09.bookmarker.domain.Bookmark;
 import io.hrushik09.bookmarker.domain.BookmarkService;
+import io.hrushik09.bookmarker.domain.BookmarksDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/bookmarks")
@@ -17,7 +15,7 @@ public class BookmarkController {
     private final BookmarkService bookmarkService;
 
     @GetMapping
-    public List<Bookmark> getBookmarks(@RequestParam(name = "page", defaultValue = "1") Integer page) {
+    public BookmarksDTO getBookmarks(@RequestParam(name = "page", defaultValue = "1") Integer page) {
         return bookmarkService.getBookmarks(page);
     }
 }
--- src/main/java/io/hrushik09/bookmarker/domain/BookmarkService.java
+++ src/main/java/io/hrushik09/bookmarker/domain/BookmarkService.java
@@ -7,8 +7,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -16,9 +14,9 @@ public class BookmarkService {
     private final BookmarkRepository bookmarkRepository;
 
     @Transactional(readOnly = true)
-    public List<Bookmark> getBookmarks(Integer page) {
+    public BookmarksDTO getBookmarks(Integer page) {
         int pageNo = page < 1 ? 0 : page - 1;
         Pageable pageable = PageRequest.of(pageNo, 10, Sort.Direction.DESC, "createdAt");
-        return bookmarkRepository.findAll(pageable).getContent();
+        return new BookmarksDTO(bookmarkRepository.findAll(pageable));
     }
 }
